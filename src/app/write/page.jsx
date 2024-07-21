05e92fc9507995b9936cb472a673e81588d5fc1c
Nurlan <iamsamurai1991@gmail.com>
2024-07-21 14:48:20 +0200

    modified:   src/app/write/page.jsx 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/globals.css 	modified:   src/app/login/page.jsx 	modified:   src/app/page.js 	modified:   src/app/userposts/page.jsx 	modified:   src/app/write/page.jsx 	src/app/post/ 	src/components/PostCard.jsx

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -2,18 +2,24 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
 const Write = () => {
   const [text, setText] = useState("");
   const handleChange = (content) => {
     setText(content);
   };
+
   return (
     <div>
+      <h2 className="text-2xl text-center mt-10">Create a new Post</h2>
       <form>
-        <input type="text" placeholder="Title" />
+        <input
+          className="w-full border my-5 p-4 outline-none"
+          type="text"
+          placeholder="Title"
+        />
       </form>
       <ReactQuill
+        className="h-[120px]"
         placeholder="What are you thinking?"
         value={text}
         onChange={handleChange}
